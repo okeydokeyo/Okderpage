a051d445f4b7227308d270e8e203c0d149971d54
mmandy2468tw <mmandy2468tw@gmail.com>
2017-07-25 13:49:07 +0800

    認識脊髓損傷-未完成

---
+++ Web/KnowMore_3.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>桃園市私立脊髓損傷潛能發展中心</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="KnowMore.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+</head>
+<body>    
+<header>
+    <?php 
+    include("top_menu.php");
+    ?>
+</header>
+
+<table cellspacing="1" cellpadding="2" border="0" id="table3">
+    <tbody>
+        <tr>
+		  <td align="left" valign="middle" id="title_01">台灣脊髓損傷者概況</td>
+		</tr>
+        <tr>
+            <td colspan="2">脊髓損傷者大多中途致殘，一夕之間失去正常人的基本能力，嚴重者甚至無法自行進食、如厠、沐浴及翻身，因此大多數脊髓損傷者意志消沈、封閉自我造成人際關係疏離；又由於受傷後無法恢復原有的工作能力，從生產者變為依賴者，且終生需要醫療照顧，對家庭造成極大的負擔。此外，嚴重的後遺病變如大小便失禁、神經根痛、體溫調節功能失常、褥瘡及性功能障礙將一輩子困擾著患者。<br /></td>
+        </tr>
+        <tr>
+            <td colspan="2">根據統計目前國內每年大約新增2500位脊髓損傷病例，受傷年齡平均在二十七歲，其中有百分之九十二的患者需一輩子坐輪椅，每位患者一生耗用的社會成本約二千五百萬。<br /></td>
+        </tr>
+    </tbody>
+</table>
+    
+<footer>
+    <?php
+    include("footer.php");
+    ?>
+</footer>
+</body>
+</html>
