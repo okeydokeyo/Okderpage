8c46728c69d67cded1a4ddc3bb2f2c2707552782
kaiokan <wilson60103@gmail.com>
2017-07-26 15:44:42 +0800

    新增留言版回覆功能

--- Web/billboard.php
+++ Web/billboard.php
@@ -29,6 +29,44 @@
                 alert("您的留言已提交，審核後即會顯示於下方留言區");
             }
         }
+        
+        function showReply(ID_in){
+           if (ID_in == "") {
+                document.getElementById("txtHint").innerHTML = "";
+                return;
+            } 
+            else { 
+                if (window.XMLHttpRequest) {
+                    xmlhttp = new XMLHttpRequest(); // code for IE7+, Firefox, Chrome, Opera, Safari
+                } 
+                else {
+                    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");   // code for IE6, IE5
+                }
+                
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById("dialog-form").innerHTML = this.responseText;
+                        $(function() {
+                            var dialog = $("#dialog-form").dialog({
+                                autoOpen: false,
+                                height: 600,
+                                width: 1000,
+                                modal: true,
+                                position: { my: 'center bottom', at: 'center center'}
+                            });
+                            dialog.dialog( "open" );
+                        });
+                        $('#sendreply_button').click(function(){
+                            $("#dialog-form").submit();
+                            alert("您的回覆已提交，審核後即會顯示於下方回覆區");
+                        });
+        
+                    }
+                };
+                xmlhttp.open("GET","dialog.php?q="+ID_in,true);
+                xmlhttp.send();
+            }  
+        }  
     </script>
 </head>
 <header>
@@ -36,7 +74,14 @@
         include("top_menu.php");
     ?>
 </header>
-<body>    
+<body>  
+    <?php
+        require_once("dbtools.inc.php");
+        $link = create_connection();
+    ?>  
+    <form id="dialog-form" method="post" title="回覆留言" style="display:none" action="reply_post.php">
+    </form>
+    
     <form name="myForm" method="post" action="billboard_post.php">
         <table width="80%" align="center" cellspacing="0" id="input_table">
             <tr>
@@ -60,7 +105,7 @@
             </tr>
             <tr align="right">
                 <td colspan="3">
-                    <input type="button" value="發布" id="sendbutton" onClick="check_data()">
+                    <input type="button" value="發布" class="sendbutton" onClick="check_data()">
                 </td>
             </tr>
         </table>
@@ -69,95 +114,74 @@
     <div class="myDiv">
         <ul>
             <?php
-            require_once("dbtools.inc.php");
-            $link = create_connection();
             $sql = "SELECT*FROM comments WHERE pass=1 ORDER BY time DESC";
             $result = execute_sql("scsrc", $sql, $link);    
             $num_rows = mysql_num_rows($result);
-            $message_count=0;
-            $i=0;
-            
-            
-            
-            while($i < $num_rows){
-                if($message_count >= 3){
+            class Message{
+                var $ID, $category, $topic, $name, $current_time, $content;
+                function __construct($ID_in, $category_in, $topic_in, $name_in, $time_in, $content_in){
+                    $this->ID = $ID_in;
+                    $this->category = $category_in;
+                    $this->topic = $topic_in;
+                    $this->name = $name_in;
+                    $this->current_time = $time_in;
+                    $this->content = $content_in;
+                }
+                function displayMessage(){
                     echo '
-                    </ul>
-                    </div>
-                    <div class="myDiv">
-                    <ul>
                         <li>
                     <table id="message_table"  cellspacing="0">
                         <tr>
-                            <td><label id="category">'.mysql_result($result,$i,4).'</label></td>
+                            <td><label id="category">'.$this->category.'</label></td>
                         </tr>
                         <tr class="message_row">
-                            <td id="td_1"><label id="topic_label">'.mysql_result($result,$i,3).'</label></td>
+                            <td id="td_1"><label id="topic_label">'.$this->topic.'</label></td>
                             <td id="td_2" rowspan="2">
-                                <label id="name_label">'.mysql_result($result,$i,2).'</label>
+                                <label class="name_label">'.$this->name.'</label>
                             </td>
                         </tr>
                         <tr class="message_row tr_2">
-                            <td><label id="time_label">'.mysql_result($result,$i,5).'</label></td>
+                            <td><label id="time_label">'.$this->current_time.'</label></td>
                         </tr>
                         <tr class="message_row tr_2">
                             <td width="100%" colspan="2">
-                                <textarea class="form-control" rows=6 readonly="readonly" id="content_area">'.mysql_result($result,$i,1).'</textarea>
+                                <textarea readonly="readonly" class="form-control content_area" rows=6 style="background-color: white;">'.$this->content.'</textarea>
                             </td>
                         </tr>
                         <tr class="message_row" align="right">
                             <td id="td_3" colspan="2">
-                                <input type="button" value="回覆" id="replybutton">
+                                <input type="button" value="回覆" id="replybutton" onClick="showReply('.$this->ID.')">
                             </td>
                         </tr>
                     </table>
                         </li>';
+                }
+            }
+            $message_count=0;
+            $i=0;
+            while($i < $num_rows){
+                $Obj = new Message(mysql_result($result,$i,0), mysql_result($result,$i,4), mysql_result($result,$i,3), mysql_result($result,$i,2), mysql_result($result,$i,5), mysql_result($result,$i,1));
+                if($message_count >= 3){
+                    echo '
+                        </ul>
+                        </div>
+                        <div class="myDiv">
+                        <ul>'; 
+                    $Obj -> displayMessage();
                     $i++;
                     $message_count = 1;
                 }
                 else{
-                    echo '<li>
-                    <table id="message_table"  cellspacing="0">
-                        <tr>
-                            <td><label id="category">'.mysql_result($result,$i,4).'</label></td>
-                        </tr>
-                        <tr class="message_row">
-                            <td id="td_1"><label id="topic_label">'.mysql_result($result,$i,3).'</label></td>
-                            <td id="td_2" rowspan="2">
-                                <label id="name_label">'.mysql_result($result,$i,2).'</label>
-                            </td>
-                        </tr>
-                        <tr class="message_row tr_2">
-                            <td><label id="time_label">'.mysql_result($result,$i,5).'</label></td>
-                        </tr>
-                        <tr class="message_row tr_2">
-                            <td width="100%" colspan="2">
-                                <textarea class="form-control" rows=6 readonly="readonly" id="content_area">'.mysql_result($result,$i,1).'</textarea>
-                            </td>
-                        </tr>
-                        <tr class="message_row" align="right">
-                            <td id="td_3" colspan="2">
-                                <input type="button" value="回覆" id="replybutton">
-                            </td>
-                        </tr>
-                    </table>
-                </li>';
+                    $Obj -> displayMessage();
                     $i++;
                     $message_count++;
                }  
             }
             mysql_free_result($result);
             mysql_close($link);
-            
-            
-            
-            function alert($msg) {
-                echo "<script type='text/javascript'>alert('$msg');</script>";
-            }
         ?>
     </ul>   
     </div>
-    
     <footer>
         <?php
             include("footer.php");
