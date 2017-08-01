418fd01be7398215901b35eed35c8c17e0943a65
WinnieLinshi <s5l4cl4@gmail.com>
2017-08-01 23:30:57 +0800

    捐贈分頁
    新增分頁

---
+++ Web/donate4.php
@@ -0,0 +1,127 @@
+<html>
+<head>
+    <title>桃園市私立脊髓損傷潛能發展中心</title>
+    <link href="images/favicon.ico" rel="SHORTCUT ICON">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="donate.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+
+</head>
+    
+<header>
+    <?php 
+    include("top_menu.php");
+    ?>
+</header>
+<div class="container">
+  <div class="content-text row-space-huge">
+     <div class="r">
+    <h1>捐款捐物徵信錄</h1>
+      </div>
+  <div class="row row-space">
+   <div align="left" style="padding:5px;margin:5px" class="text_12px_01b border_02">
+		<img src="pin.png" alt="*" width="10" height="11" align="absmiddle"> 選擇檔案類別：
+	<select id="DB_DowUnitID" name="DB_DowUnitID" class="text_12px_01b">
+		<option value="">不分類</option>
+					
+		<option value="22">捐款徵信</option>
+					
+		<option value="25">捐物徵信</option>
+				
+	</select>
+	<input name="no" type="hidden" value="12">
+		<input type="submit" name="name" value="送出" id="name" class="text_12px_01b">
+	    </div>
+    <table style="border:3px #cccccc solid;" cellpadding="5" rules="all"  width="100%" border="0" id="margin_01" class="text_12px_01" summary="捐款捐物徵信錄表格">
+		  <tbody><tr>
+            <th width="10%" align="center" class="th2">序號</th>
+            <th width="70%" align="center" class="th2">檔案名稱</th>
+            <th width="20%" align="center" class="th2-end">檔案</th>
+          </tr>
+		  <tr class="a">
+		  </tr>
+				  <tr class="a">
+			<td align="center" valign="middle" class="border_02">1</td>
+			<td align="center" valign="middle" class="text_12px_01 border_02"><a href="徵信/捐款人徵信查詢方式.pdf" class="link_01" title="捐款人徵信查詢方式.pdf(另開視窗下載)">捐款人徵信查詢方式</a></td>
+			<td align="center" valign="middle" class="border_02"><a href="download.php?DB_FileTitle=%E6%8D%90%E6%AC%BE%E4%BA%BA%E5%BE%B5%E4%BF%A1%E6%9F%A5%E8%A9%A2%E6%96%B9%E5%BC%8F.pdf&amp;DB_FileName=1466559617.pdf" class="link_01" title="捐款人徵信查詢方式.pdf(另開視窗下載)">直接下載</a></td>
+		  </tr>
+				  <tr>
+			<td align="center" valign="middle" class="border_02">2</td>
+			<td align="center" valign="middle" class="text_12px_01 border_02"><a href="徵信/捐款徵信/106.pdf" class="link_01" title="106捐款徵信1-6月.pdf(另開視窗下載)">106年捐款徵信</a></td>
+			<td align="center" valign="middle" class="border_02"><a href="download.php?DB_FileTitle=106%E6%8D%90%E6%AC%BE%E5%BE%B5%E4%BF%A11-6%E6%9C%88.pdf&amp;DB_FileName=1500511884.pdf" class="link_01" title="106捐款徵信1-6月.pdf(另開視窗下載)">直接下載</a></td>
+		  </tr>
+				  <tr>
+			<td align="center" valign="middle" class="border_02">3</td>
+			<td align="center" valign="middle" class="text_12px_01 border_02"><a href="徵信/捐物徵信/106.pdf" class="link_01" title="106年捐物徵信1060718.pdf(另開視窗下載)">106年捐物徵信</a></td>
+			<td align="center" valign="middle" class="border_02"><a href="download.php?DB_FileTitle=106%E5%B9%B4%E6%8D%90%E7%89%A9%E5%BE%B5%E4%BF%A11060718.pdf&amp;DB_FileName=1500368683.pdf" class="link_01" title="106年捐物徵信1060718.pdf(另開視窗下載)">直接下載</a></td>
+		  </tr>
+				  <tr>
+			<td align="center" valign="middle" class="border_02">4</td>
+			<td align="center" valign="middle" class="text_12px_01 border_02"><a href="徵信/捐款徵信/105.pdf" class="link_01" title="105捐款徵信.pdf(另開視窗下載)">105年捐款徵信</a></td>
+			<td align="center" valign="middle" class="border_02"><a href="download.php?DB_FileTitle=105%E6%8D%90%E6%AC%BE%E5%BE%B5%E4%BF%A1.pdf&amp;DB_FileName=1485256134.pdf" class="link_01" title="105捐款徵信.pdf(另開視窗下載)">直接下載</a></td>
+		  </tr>
+				  <tr>
+			<td align="center" valign="middle" class="border_02">5</td>
+			<td align="center" valign="middle" class="text_12px_01 border_02"><a href="徵信/捐物徵信/105.pdf" class="link_01" title="105捐物徵信.pdf(另開視窗下載)">105年捐物徵信</a></td>
+			<td align="center" valign="middle" class="border_02"><a href="download.php?DB_FileTitle=105%E6%8D%90%E7%89%A9%E5%BE%B5%E4%BF%A1.pdf&amp;DB_FileName=1485256415.pdf" class="link_01" title="105捐物徵信.pdf(另開視窗下載)">直接下載</a></td>
+		  </tr>
+				  <tr>
+			<td align="center" valign="middle" class="border_02">6</td>
+			<td align="center" valign="middle" class="text_12px_01 border_02"><a href="徵信/捐物徵信/104.pdf" class="link_01" title="104年捐物徵信1060720更新.pdf(另開視窗下載)">104年捐物徵信</a></td>
+			<td align="center" valign="middle" class="border_02"><a href="download.php?DB_FileTitle=104%E5%B9%B4%E6%8D%90%E7%89%A9%E5%BE%B5%E4%BF%A11060720%E6%9B%B4%E6%96%B0.pdf&amp;DB_FileName=1500512974.pdf" class="link_01" title="104年捐物徵信1060720更新.pdf(另開視窗下載)">直接下載</a></td>
+		  </tr>
+				  <tr>
+			<td align="center" valign="middle" class="border_02">7</td>
+			<td align="center" valign="middle" class="text_12px_01 border_02"><a href="徵信/捐款徵信/104.pdf" class="link_01" title="104捐款徵信.pdf(另開視窗下載)">104年捐款徵信</a></td>
+			<td align="center" valign="middle" class="border_02"><a href="download.php?DB_FileTitle=104%E6%8D%90%E6%AC%BE%E5%BE%B5%E4%BF%A1.pdf&amp;DB_FileName=1464332242.pdf" class="link_01" title="104捐款徵信.pdf(另開視窗下載)">直接下載</a></td>
+		  </tr>
+				  <tr>
+			<td align="center" valign="middle" class="border_02">8</td>
+			<td align="center" valign="middle" class="text_12px_01 border_02"><a href="徵信/捐物徵信/103.pdf" class="link_01" title="103年捐物徵信.pdf(另開視窗下載)">103年捐物徵信</a></td>
+			<td align="center" valign="middle" class="border_02"><a href="download.php?DB_FileTitle=103%E5%B9%B4%E6%8D%90%E7%89%A9%E5%BE%B5%E4%BF%A1.pdf&amp;DB_FileName=1479294613.pdf" class="link_01" title="103年捐物徵信.pdf(另開視窗下載)">直接下載</a></td>
+		  </tr>
+				  <tr>
+			<td align="center" valign="middle" class="border_02">9</td>
+			<td align="center" valign="middle" class="text_12px_01 border_02"><a href="徵信/捐款徵信/103.pdf" class="link_01" title="103年捐款徵信.pdf(另開視窗下載)">103年捐款徵信</a></td>
+			<td align="center" valign="middle" class="border_02"><a href="download.php?DB_FileTitle=103%E5%B9%B4%E6%8D%90%E6%AC%BE%E5%BE%B5%E4%BF%A1.pdf&amp;DB_FileName=1421030173.pdf" class="link_01" title="103年捐款徵信.pdf(另開視窗下載)">直接下載</a></td>
+		  </tr>
+				  <tr>
+			<td align="center" valign="middle" class="border_02">10</td>
+			<td align="center" valign="middle" class="text_12px_01 border_02"><a href="徵信/捐物徵信/102.pdf" class="link_01" title="102年捐物徵信.pdf(另開視窗下載)">102年捐物徵信</a></td>
+			<td align="center" valign="middle" class="border_02"><a href="download.php?DB_FileTitle=102%E5%B9%B4%E6%8D%90%E7%89%A9%E5%BE%B5%E4%BF%A1.pdf&amp;DB_FileName=1401932764.pdf" class="link_01" title="102年捐物徵信.pdf(另開視窗下載)">直接下載</a></td>
+		  </tr>
+				</tbody></table>
+				
+								
+				<div align="left" style="padding:5px;margin:5px"><img src="pin.png" alt="*" width="5" height="5" align="absmiddle"> <span class="text_12px_04">資料筆數：17　　
+				
+		頁數：1 / 2</span>
+		　|　
+		  <a href="javascript:GoPage(1)" title="最前頁" class="button_05">|&lt;</a>
+		  <a href="javascript:GoPage(1)" title="上一頁" class="button_05">&lt;&lt;</a>
+		  		  
+		       　<span class="text_12px_03b">1</span>		  		  
+		       　<a href="javascript:GoPage(2)" class="link_05" title="第2頁">2</a>		  　		  
+		  <a href="javascript:GoPage(2)" title="下一頁" class="button_05">&gt;&gt;</a>
+		  <a href="javascript:GoPage(2)" title="最後頁" class="button_05">&gt;|</a>
+					
+		</div>
+				</div></div></div>
+
+<footer>
+    <?php
+    include("footer.php");
+    ?>
+</footer>
+</body>
+</html>
