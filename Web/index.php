c720ac8d9b604802bb73beb71ee0c2d3f3679d1e
WinnieLinshi <s5l4cl4@gmail.com>
2017-08-31 22:39:33 +0800

    最新消息更改
    改成引用資料庫

--- Web/index.php
+++ Web/index.php
@@ -52,114 +52,44 @@
     </a></div>
 </div>
   <div class="box-b">
-   <table cellpadding='5' width="100%" border="0" cellspacing="0" cellpadding="0" id="margin_01" summary="最新消息文字表格">
-		   		<tbody><tr>
-		  <td width="1%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-06-23</td>
-		  <td align="left" valign="middle" class="border_02"><a href="news_com.php?no=1400" class="link_03" title="【活動資訊】Love Ring The World 「健康桃園 讓愛髓行」公益健走 開放報名">【活動資訊】Love Ring The World 「健康桃園 讓愛髓行」公益健走 開放報名</a></td>
+		  	  	  <table width="100%" border="0" cellspacing="0" cellpadding="0" id="margin_01" summary="<? echo $Inter_ary['DB_IntSubject'];?>文字表格">
+		   <?		   
+		   //查詢條例式訊息管理資料
+		   $time = date("Y-m-d"); //時間
+		   $Ordi_result = mysql_query("select * from `ordi` where `DB_OrdTagID`='".$Inter_ary['DB_IntNumID']."' && `DB_OrdAnnounce`='0' && (`DB_OrdStart_Time`<='$time' && `DB_OrdEnd_Time`>='$time' || `DB_OrdPermanent`='1')  ORDER BY `DB_OrdTime` DESC LIMIT 0,".$Inter_ary['DB_IntOrdi']."");
+		   $Ordi_num = mysql_num_rows($Ordi_result);
+		   
+		   if ($Ordi_num <> 0 && $ordt_ary['DB_OrdTagID'] != ""){
+		   		
+				while ( $Ordi_ary = mysql_fetch_array($Ordi_result) ){
+				
+		         if ($Ordi_ary['DB_OrdBasis'] == "1"){
+				             $newClass = "news_com.php?no=".$Ordi_ary['DB_OrdID']."";
+							 $newName = $Ordi_ary['DB_OrdSubject'];
+							 $newOpen = "";
+				 }else if ($Ordi_ary['DB_OrdBasis'] == "2"){
+				             $newClass = "download.php?DB_FileTitle=".$Ordi_ary['DB_OrdName2_1']."&DB_FileName=".$Ordi_ary['DB_OrdArchive2_1']."";
+							 $newName = "".$Ordi_ary['DB_OrdName2_1']."(點擊下載)";
+							 $newOpen = "";
+				 }else if ($Ordi_ary['DB_OrdBasis'] == "3"){
+				             $newClass = $Ordi_ary['DB_OrdUrl3_1'];
+							 $newName = "".$Ordi_ary['DB_OrdSubject']."(另開新視窗)";
+							 $newOpen = "target='_blank'";
+				 }
+
+		   ?>
+		<tr>
+		  <td width="1%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3" /></td>
+		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02"><? echo $Ordi_ary['DB_OrdTime'];?></td>
+		  <td align="left" valign="middle" class="border_02"><a href="<? echo $newClass;?>" class="link_03" title="<? echo $newName;?>" <? echo $newOpen;?>><? echo $Ordi_ary['DB_OrdSubject'];?></a></td>
 		</tr>
 		<tr>
 		  <td colspan="3" class="h5"></td>
 		</tr>
-		  		<tr>
-		  		  <td width="3%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-06-22</td>
-		  <td align="left" valign="middle" class="border_02"><a href="news_com.php?no=1030" class="link_03" title="【大愛電視台】「甘願人生」轉動的愛-林進興">【大愛電視台】「甘願人生」轉動的愛-林進興</a></td>
-		</tr>
-		<tr>
-		  <td colspan="3" class="h5"></td>
-		</tr>
-		  		<tr>
-		  <td width="3%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-06-21</td>
-		  <td align="left" valign="middle" class="border_02"><a href="news_com.php?no=1396" class="link_03" title="【招募志工】施比受更有福、予比取更喜樂">【招募志工】施比受更有福、予比取更喜樂</a></td>
-		</tr>
-		<tr>
-		  <td colspan="3" class="h5"></td>
-		</tr>
-		  		<tr>
-		  <td width="3%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-06-21</td>
-		  <td align="left" valign="middle" class="border_02"><a href="news_com.php?no=1414" class="link_03" title="【中國時報】職訓重生 脊損傷友回歸社會">【中國時報】職訓重生 脊損傷友回歸社會</a></td>
-		</tr>
-		<tr>
-		  <td colspan="3" class="h5"></td>
-		</tr>
-		  		<tr>
-		  <td width="3%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-06-20</td>
-		  <td align="left" valign="middle" class="border_02"><a href="news_com.php?no=664" class="link_03" title="【招生訊息】台灣大哥大客服工作室招生">【招生訊息】台灣大哥大客服工作室招生</a></td>
-		</tr>
-		<tr>
-		  <td colspan="3" class="h5"></td>
-		</tr>
-		  		<tr>
-		  <td width="3%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-06-20</td>
-		  <td align="left" valign="middle" class="border_02"><a href="news_com.php?no=1413" class="link_03" title="【聯合報】兩次撿回一命 她體會該為所愛的人及自己好好活">【聯合報】兩次撿回一命 她體會該為所愛的人及自己好好活</a></td>
-		</tr>
-		<tr>
-		  <td colspan="3" class="h5"></td>
-		</tr>
-		  		<tr>
-		  <td width="3%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-06-20</td>
-		  <td align="left" valign="middle" class="border_02"><a href="news_com.php?no=1416" class="link_03" title="脊髓損傷潛能發展中心-頸髓社區家園啟用介紹 ">脊髓損傷潛能發展中心-頸髓社區家園啟用介紹 </a></td>
-		</tr>
-		<tr>
-		  <td colspan="3" class="h5"></td>
-		</tr>
-		  		<tr>
-		  <td width="3%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-06-19</td>
-		  <td align="left" valign="middle" class="border_02"><a href="news_com.php?no=1120" class="link_03" title="【招生訊息】106年度基礎電腦班持續招生中~~~">【招生訊息】106年度基礎電腦班持續招生中~~~</a></td>
-		</tr>
-		<tr>
-		  <td colspan="3" class="h5"></td>
-		</tr>
-		  		<tr>
-		  <td width="3%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-06-16</td>
-		  <td align="left" valign="middle" class="border_02"><a href="https://issuu.com/adamstalk/docs/____________________________________5f094bc6e5442f" class="link_03" title="【刊物發行】友善新世界雙月刊 第九期 歡迎線上閱讀 支持公益響應環保(另開新視窗)" target="_blank">【刊物發行】友善新世界雙月刊 第九期 歡迎線上閱讀 支持公益響應環保</a></td>
-		</tr>
-		<tr>
-		  <td colspan="3" class="h5"></td>
-		</tr>
-		  		<tr>
-		  <td width="3%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-05-31</td>
-		  <td align="left" valign="middle" class="border_02"><a href="news_com.php?no=1411" class="link_03" title="【人間衛視】林堉琪基金會辦愛心園遊會 圓山登場">【人間衛視】林堉琪基金會辦愛心園遊會 圓山登場</a></td>
-		</tr>
-		<tr>
-		  <td colspan="3" class="h5"></td>
-		</tr>
-		  		<tr>
-		  <td width="3%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-05-28</td>
-		  <td align="left" valign="middle" class="border_02"><a href="news_com.php?no=1410" class="link_03" title="【自由時報】林堉琪基金會 助脊損者重建">【自由時報】林堉琪基金會 助脊損者重建</a></td>
-		</tr>
-		<tr>
-		  <td colspan="3" class="h5"></td>
-		</tr>
-		  		<tr>
-		  <td width="3%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-05-27</td>
-		  <td align="left" valign="middle" class="border_02"><a href="news_com.php?no=1405" class="link_03" title="【娛樂新聞】助脊髓愻損傷者！「讓生命發光」園遊會 27日花博園區登場">【娛樂新聞】助脊髓损伤者！「让生命发光」园游会 27日花博园区登场</a></td>
-		</tr>
-		<tr>
-		  <td colspan="3" class="h5"></td>
-		</tr>
-		  		<tr>
-		  <td width="3%" align="center" valign="top" id="padding_07" class="border_02"><img src="images/icon_point.gif" alt="*" width="3" height="3"></td>
-		  <td width="12%" align="left" valign="top" class="text_12px_06 border_02">2017-05-27</td>
-		  <td align="left" valign="middle" class="border_02"><a href="news_com.php?no=1407" class="link_03" title="【中華日報】年增2000脊損傷友 均齡27">【中華日報】年增2000脊損傷友 均齡27<div style="width:13%; float:right;">  
-						<a href="news_list.php?no=10" title="更多(最新消息)" class="link_03"><img src="images/more_01.gif" alt="more" width="39" height="17" border="0"></a>
-			   			</div></a></td>
-		</tr>
-		<tr>
-		  <td colspan="3" class="h5"></td>
-		</tr>
-		  	  </tbody></table></div>
+		  <? }}?>
+	  </table>
+		  	  
+		  	  </div>
     </div>
 <div class="row">
     <div class="col-xs-12 col-md-10 col-md-offset-1">
