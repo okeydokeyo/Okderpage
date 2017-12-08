844c2cdf8c9492a96ab5d639a4b153961a6ea45a
kaiokan <wilson60103@gmail.com>
2017-12-08 21:32:44 +0600

    留言板修復

--- Web/billboard_post.php
+++ Web/billboard_post.php
@@ -1,32 +1,32 @@
 <?php
-    require_once("dbtools.inc.php");
-    
-    $content = $_POST["content"];
-    $name = $_POST["name"];
-    $topic = $_POST["topic"];
-    switch($_POST["category"]){
-        case "donation":
-            $category = "捐款/捐物";
-            break;
-        case "report":
-            $category = "通報";
-            break;
-        case "injury":
-            $category = "脊髓損傷";
-            break;
-        case "information":
-            $category = "中心相關問題";
-            break;
-    }
-    $email = $_POST["email"];
-    date_default_timezone_set('Asia/Taipei');   
-    $current_time = date("Y-m-d H:i:s");
+include "config.php";
 
-    $link = create_connection();
-    $sql = "INSERT INTO comments(DB_MesName, DB_MesSubject, DB_MesContent, category, DB_MesTime, DB_MesEmail) VALUES('$name', '$topic', '$content', '$category', '$current_time', '$email')";
-    $result = execute_sql("scsrc2", $sql, $link);
+$content = $_POST["content"];
+$name = $_POST["name"];
+$topic = $_POST["topic"];
+switch($_POST["category"]){
+    case "donation":
+    $category = "捐款/捐物";
+    break;
     
-    mysqli_close($link);
-    header("location:billboard.php");
-    exit(); 
+    case "report":
+    $category = "通報";
+    break;
+        
+    case "injury":
+    $category = "脊髓損傷";
+    break;
+    
+    case "information":
+    $category = "中心相關問題";
+    break;
+}
+$email = $_POST["email"];
+date_default_timezone_set('Asia/Taipei');   
+$current_time = date("Y-m-d H:i:s");
+
+$sql = "INSERT INTO comments(DB_MesName, DB_MesSubject, DB_MesContent, category, DB_MesTime, DB_MesEmail) VALUES('$name', '$topic', '$content', '$category', '$current_time', '$email')";
+mysql_query($sql);
+header("location:billboard.php");
+exit(); 
 ?>
